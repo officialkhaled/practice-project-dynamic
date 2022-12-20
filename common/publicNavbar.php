1bb9b6120391b567418b33db960b3b0deac2f683
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2022-12-20 21:55:25 +0600

    restart

--- common/publicNavbar.php
+++
@@ -1,9 +0,0 @@
-<!-- Public Navbar -->
-<div>
-  <a href="../views/publicHome.php">Home</a>
-  <a href="../views/login.php">Login</a>
-  <a href="../views/register.php">Registration</a>
-  <a href="../views/deals&offers_pub.php">Deals/Offers</a>
-  <a href="../views/counters.php">Counters</a>
-  <a href="../views/contactUs.php">Contact Us</a>
-</div>
--- models/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  $con = mysqli_connect('localhost', 'root', '', 'registration');
-
-  if (!$con) {
-    die(mysqli_error($con));
-  }
-
-?>
